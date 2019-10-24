6507091359a100862c8270dbeeba841528e3acde
AAA <gevork_ssh@mail.ru>
2019-10-24 22:51:28 +0200

    My first commit!

--- lection8/task8-11.py
+++ lection8/task8-11.py
@@ -11,3 +11,26 @@
 60276311 55.5695795 37.5764692
 60276312 55.5682232 37.5754573
 """
+from bs4 import BeautifulSoup as BS
+"""
+from urllib.request import urlopen
+url='https://stepik.org/media/attachments/lesson/266078/mapcity.osm'
+response = urlopen(url)
+xml = response.read().decode('utf8')
+"""
+fout=open('out8-11.txt','w',encoding='utf8')
+xml=open('mapcity.osm','r',encoding='utf8').read()
+soup = BS(xml, 'lxml')
+cnt=100
+i=0
+#nodes = {}#нам понадобятся все ноды
+for node in soup.find_all('node'):
+    if i<cnt:
+        lat=node['lat']
+        lon=node['lon']
+        id=node['id']
+        print(id,lat,lon,file=fout)
+        i+=1
+    else:
+        break
+fout.close()
