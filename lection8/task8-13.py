4c4cd3948b701d8cc3535a8297ce42e9c76c62ad
AAA <gevork_ssh@mail.ru>
2019-10-24 23:04:50 +0200

    My first commit!

--- lection8/task8-13.py
+++ lection8/task8-13.py
@@ -16,3 +16,25 @@
 28911067
 317558736 529559432 317558559 317558560 317558561 317558562 317558290 529559420 529559416 529559414 529559412 529559410 529559426 529559424 529559422 317558289 317558288 317558736
 """
+from bs4 import BeautifulSoup as BS
+"""
+from urllib.request import urlopen
+url='https://stepik.org/media/attachments/lesson/266078/mapcity.osm'
+response = urlopen(url)
+xml = response.read().decode('utf8')
+"""
+
+xml=open('mapcity.osm','r',encoding='utf8').read()
+soup = BS(xml, 'lxml')
+for way in soup.find_all('way'):
+    flag = False  # вдруг "дорога" - не хайвей
+    for tag in way('tag'):
+        if tag['k'] == 'building':
+            # нам надо узнать все id нодов, входящих в этот way
+            rnodes = []  # список нодов
+            for nd in way('nd'):
+                ref = nd['ref']
+                rnodes.append(ref)
+            if rnodes[0]==rnodes[len(rnodes)-1]:
+                print(way['id'])
+                print(*rnodes)
