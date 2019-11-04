3356af5201c1f7ce5340c615532b69c712b69c92
AAA <gevork_ssh@mail.ru>
2019-11-04 10:58:35 +0200

    My first commit!

---
+++ lection10/task10-1.py
@@ -0,0 +1,15 @@
+"""
+В этой задаче вам предстоит установить и проверить модуль xmltodict.
+Для этого скачайте xml-файл, а затем запустите приведенную ниже программу в той же папке, куда сохранен файл:
+
+"""
+import xmltodict
+
+fin = open('test10-1.xml', 'r', encoding='utf8')
+xmltext = fin.read()
+fin.close()
+xml = xmltodict.parse(xmltext)
+for plant in xml['CATALOG']['PLANT']:
+    for key in plant:
+        print(key, plant[key])
+    print('---')
